316114f5a7509c95935edfa7149ecacea928d0a5
ijasbrat <146550669+ijasbrat@users.noreply.github.com>
2025-10-13 12:42:38 +0530

    Add files via upload

---
+++ Day13-20/ConsoleApp1/Lambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Lambda & LINQ Demo ");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+            Console.WriteLine("Even Numbers:");
+            evenNumbers.ForEach(n => Console.WriteLine(n));
+            Console.WriteLine(" Lambda demo completed.");
+        }
+    }
+}
