864ef74c644e92da06598f21a58a1a6fdce0a6d3
ijasbrat <146550669+ijasbrat@users.noreply.github.com>
2025-10-13 12:08:14 +0530

    Add files via upload

---
+++ Day13-20/ConsoleApp1/NumberGuessing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I have chosen a number between 1 and 100.");
+            Console.WriteLine("Try to guess it!");
+            Console.WriteLine();
+            Random random = new Random();
+            int target = random.Next(1, 101); 
+            int guess = 0;                    
+            int attempts = 0;                 
+            while (guess != target)
+            {
+                Console.Write("Enter your guess (1–100): ");
+                string input = Console.ReadLine()
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine(" Please enter a valid number!");
+                    continue; 
+                }
+                attempts++;
+                if (guess < target)
+                {
+                    Console.WriteLine("Too low! Try again.\n");
+                }
+                else if (guess > target)
+                {
+                    Console.WriteLine("Too high! Try again.\n");
+                }
+                else
+                {
+                    
+                    Console.WriteLine($"\n Congratulations! You guessed the number {target} correctly!");
+                    Console.WriteLine($"It took you {attempts} attempts.");
+                }
+            }
+            Console.WriteLine("\nThanks for playing! Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
